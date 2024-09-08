5495c00156fe9f2beddbbc8f83d207c27cd8e9cb
Doherty <gabriel.dcarvalho@al.infnet.edu.br>
2024-09-07 23:32:18 -0300

    Adicionar arquivos de projeto.

---
+++ TP3/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP3.Models;
+
+namespace TP3.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ TP3/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP3.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Preco { get; set;}
+    }
+}
